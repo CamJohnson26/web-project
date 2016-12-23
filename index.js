a0fbdd21461774ae0195c95f3d47bf714b2b0a94
Sha-8ang <shabang3533@gmail.com>
2016-12-23 16:51:00 -0700

    polyer added!

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var express = require('express')
-var app = express()
+'use strict';
 
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
+let express = require('express');
+let app = express();
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send("\
+  response.send('\
 https://github.com/CamJohnson26/web-project<br>\
 Github -> CodeShip -> AWS\
 \
 Listens for a commit to master and triggers a codeship build. Deploys to AWS and runs the scripts in Scripts, based on instructions in appspec.yml.\
 \
 index.js running at http://35.164.104.178/\
-")
-})
+');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log('Node app is running at localhost:' + app.get('port'));
+});
