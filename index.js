d19a9e61e563d3b30c78ef90eb8c97c43d0e3570
Nathan Huff <nthuff@gmail.com>
2017-02-08 20:13:04 -0700

    updates, but still broken

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-'use strict';
+(() => {
+    'use strict';
 
-var express = require('express');
-var app = express();
+    const express = require('express');
+    const app = express();
+    const server = require('http').Server(app);
+    const io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 8080));
-app.use(express.static(__dirname + '/public'));
+    app.set('port', (process.env.PORT || 8080));
+    // app.get('/', function(request, response) {
+    //   response.send('\
+    // https://github.com/CamJohnson26/web-project<br>\
+    // Github -> CodeShip -> AWS\
+    // \
+    // Listens for a commit to master and triggers a codeship build.
+    // Deploys to AWS and runs the scripts in Scripts, based on instructions in
+    // appspec.yml.\
+    // \
+    // index.js running at http://35.164.104.178/\
+    // ');
+    // });
 
-app.get('/', function(request, response) {
-  response.send('\
-https://github.com/CamJohnson26/web-project<br>\
-Github -> CodeShip -> AWS\
-\
-Listens for a commit to master and triggers a codeship build. Deploys to AWS and runs the scripts in Scripts, based on instructions in appspec.yml.\
-\
-index.js running at http://35.164.104.178/\
-');
-});
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running at localhost:' + app.get('port'));
+    });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running at localhost:' + app.get('port'));
-});
+    io.sockets.on('connection', function(socket) {
+        socket.emit('test', 'testMessage');
+        socket.on('time now there', function() {
+            console.log('time now there');
+            socket.emit('time now server', new Date());
+        });
+    });
+    app.use(express.static(__dirname + '/public'));
+})();
