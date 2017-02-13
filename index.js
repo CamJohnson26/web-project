21fb0084e02a7ab94505e9c55fa71c11db8506c7
Nathan Huff <nthuff@gmail.com>
2017-02-13 00:49:34 -0700

    fake db stuff

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
   const app = express();
   const server = require('http').createServer(app);
   const io = require('socket.io')(server);
+  let names = []; // lets call this a non-persistant database
 
 
   app.set('port', (process.env.PORT || 8080));
@@ -15,6 +16,16 @@
       console.log('time now there');
       socket.emit('time now server', new Date());
     });
+    socket.on('newName', (name) => {
+      if(typeof name !== 'string') return Promise.reject();
+      names.push.apply(names, [name]);
+      if(names.length > 10) {
+        names = names.slice(names.length - 10);
+      }
+      console.log('coool names is now', names, name);
+      socket.emit('new name list', names);
+    });
+    socket.on('get name list', () => socket.emit('new name list', names));
   });
   app.use(express.static(__dirname + '/public'));
 
